f0978ba91010ad827acdf08c86e7f821ab35bfa9
Nathan Friel <nathanfriel97@gmail.com>
2024-12-05 23:53:07 -0500

    misc

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 main: src/main.cc
-	clang++ src/main.cc -Idependencies -Ldependencies/glfw -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit
+	clang++ src/main.cc dependencies/glad/src/glad.c -Idependencies -Idependencies/glad/include -Ldependencies/glfw -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit
 	./a.out
 	rm a.out
--- src/main.cc
+++ src/main.cc
@@ -1,36 +1,44 @@
 #define GL_SILENCE_DEPRECATION
 
+#include "glad/glad.h"
 #include "glfw/glfw3.h"
 #include <iostream>
 
 int main(void)
 {
-    GLFWwindow* window;
+    glfwInit();
 
-    if (!glfwInit())
-        return -1;
+    // Versions of OpenGL required (or is it versions of GLFW?)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window) {
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Necessary on mac
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Prevent window resizing
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow *window = glfwCreateWindow(600, 400, "OpenGL", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "glfwCreateWindow error\n";
         glfwTerminate();
-        std::cout << "glfwCreateWindow error" << std::endl;
+        return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout <<"gladLoadGLLoader error\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    // Main loop
     while (!glfwWindowShouldClose(window)) {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
     glfwTerminate();
-    return 0;
 }
